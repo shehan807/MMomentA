aad642e166569e7c4895f6a32b55e733dd74ce24
Shehan <parmar@gatech.edu>
2025-10-05 22:51:47 -0700

    fixing conda env

--- MMomentA/__init__.py
+++ MMomentA/__init__.py
@@ -5,12 +5,12 @@ fitting (MPFIT) for polarizable force fields, combining quantum chemistry
 calculations with graph neural network architectures.
 """
 
-from . import comparison
 from . import utils
 
 # Optional QM imports (require OpenFF/Psi4)
 try:
     from . import qm
+    from . import comparison
     _HAS_QM = True
 except ImportError:
     _HAS_QM = False
@@ -25,9 +25,9 @@ except ImportError:
     _HAS_ML = False
 
 # Build __all__ based on what's available
-__all__ = ["comparison", "utils"]
+__all__ = ["utils"]
 if _HAS_QM:
-    __all__.append("qm")
+    __all__.extend(["qm", "comparison"])
 if _HAS_ML:
     __all__.extend(["data", "models", "training"])
 
